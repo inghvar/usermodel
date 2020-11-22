ca62f0a5befa0876108248da6936292a4e69dc6e
Ihar Ilyin <info@arahna.de>
2020-11-22 03:02:17 +0300

    add README

---
+++ README.md
@@ -0,0 +1,18 @@
+# Simple app that implements login/logout, user creation and a list of all users on Django Rest Framework and Vue.js (Vuetify)
+
+# To run server:
+
+- create a virtual enviroment
+
+- pip install -r requirements.txt
+
+- cd gcode_server && python manage.py runserver
+
+
+# To run client:
+
+- cd gcode_frontend
+
+- npm install
+
+- npm run serve
--- gcode_server/users/views.py
+++ gcode_server/users/views.py
@@ -2,18 +2,16 @@ from django.contrib.auth import authenticate, get_user_model
 
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.views import APIView
-from rest_framework.permissions import AllowAny
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework import status, generics
 from rest_framework.authtoken.models import Token
-from rest_framework.permissions import IsAuthenticated
 
 from .serializers import *
 
 
 CustomUser = get_user_model()
-
 
 @api_view(["POST"])
 @permission_classes((AllowAny,))
